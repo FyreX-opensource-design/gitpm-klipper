3038c46fce2c411c8c09cac84389574b8513cc37
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-17 11:43:09 -0600

    Create install-system.sh

---
+++ install-system.sh
@@ -0,0 +1,3 @@
+sudo git clone https://github.com/Anonoei/klipper_auto_speed
+sudo chmod +x /opt/apps/klipped_auto_speed/klipper_auto_speed/install.sh
+sudo /opt/apps/klipped_auto_speed/klipper_auto_speed/install.sh
